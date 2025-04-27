925969afd9401ac946759ab1ac74c904d4df7885
BrunStu <b.sturmer4@gmail.com>
2025-04-27 20:19:22 +0200

    Added vhdl code

---
+++ vhdl/sodar_19_cp.srcs/sources_1/bd/sodar_19_cp/ip/sodar_19_cp_pwm_top_0_0/sodar_19_cp_pwm_top_0_0_stub.v
@@ -0,0 +1,24 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
+// Date        : Thu Apr 24 09:48:48 2025
+// Host        : Bruno-Komp-MSI running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub {C:/Users/plywa/Digital designe
+//               vivado/sodar_19_cp/sodar_19_cp.srcs/sources_1/bd/sodar_19_cp/ip/sodar_19_cp_pwm_top_0_0/sodar_19_cp_pwm_top_0_0_stub.v}
+// Design      : sodar_19_cp_pwm_top_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg484-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "pwm_top,Vivado 2019.1" *)
+module sodar_19_cp_pwm_top_0_0(clk, duty_cycle, period, phase, pwm_out)
+/* synthesis syn_black_box black_box_pad_pin="clk,duty_cycle[31:0],period[31:0],phase[31:0],pwm_out[18:0]" */;
+  input clk;
+  input [31:0]duty_cycle;
+  input [31:0]period;
+  input [31:0]phase;
+  output [18:0]pwm_out;
+endmodule
